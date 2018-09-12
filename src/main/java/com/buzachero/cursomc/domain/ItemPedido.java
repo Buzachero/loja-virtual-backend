35621f1673b0d8390eee3d17c52908fc1e5c9955
Henrique Buzachero <henrique_buzachero@hotmail.com>
2018-09-12 18:59:40 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/buzachero/cursomc/domain/ItemPedido.java
+++ src/main/java/com/buzachero/cursomc/domain/ItemPedido.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class ItemPedido implements Serializable {
 	/**
@@ -12,7 +15,8 @@ public class ItemPedido implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	@EmbeddedId
+	@EmbeddedId	
+	@JsonIgnore
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
 	private Double desconto;
@@ -32,10 +36,11 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+		
 	public Produto getProduto() {
 		return id.getProduto();
 	}
