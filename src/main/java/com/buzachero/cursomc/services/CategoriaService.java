7f5fec85febb616ff528cd146e3e6766f1f54d76
buzachero <buzachero@buzachero-K46CM>
2018-09-13 00:55:46 -0300

    Inserindo uma nova categoria com POST

--- src/main/java/com/buzachero/cursomc/services/CategoriaService.java
+++ src/main/java/com/buzachero/cursomc/services/CategoriaService.java
@@ -13,11 +13,18 @@ import com.buzachero.cursomc.services.exceptions.ObjectNotFoundException;
 public class CategoriaService {
 	
 	@Autowired
-	private CategoriaRepository catRepo;
+	private CategoriaRepository categoriaRepository;
 	
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = catRepo.findById(id);
+		Optional<Categoria> obj = categoriaRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
+	
 }
