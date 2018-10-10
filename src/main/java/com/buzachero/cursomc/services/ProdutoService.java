2ea84c918c2af5589db4f486183fae09c5b9e12d
Buzachero <henrique_buzachero@hotmail.com>
2018-10-09 23:41:18 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/buzachero/cursomc/services/ProdutoService.java
+++ src/main/java/com/buzachero/cursomc/services/ProdutoService.java
@@ -1,11 +1,17 @@
 package com.buzachero.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.buzachero.cursomc.domain.Categoria;
 import com.buzachero.cursomc.domain.Produto;
+import com.buzachero.cursomc.repositories.CategoriaRepository;
 import com.buzachero.cursomc.repositories.ProdutoRepository;
 
 @Service
@@ -14,8 +20,17 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository produtoRepo;
 	
-	public Produto buscar(Integer id) {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
 		Optional<Produto> obj = produtoRepo.findById(id);
 		return obj.orElse(null);
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return produtoRepo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
 	}
 }
